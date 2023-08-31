769b7ae32ef1f8d9987b847caadce34fe20935e4
VincenzoMarcello <vincenzo.marcello.92@gmail.com>
2023-08-31 16:27:08 +0200

    feat: add structure files and base template

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      todoList: [
+        {
+          text: "fare colazione",
+          done: false,
+        },
+        {
+          text: "andare in palestra",
+          done: false,
+        },
+        {
+          text: "portare fuori il cane",
+          done: false,
+        },
+        {
+          text: "vedere un film",
+          done: false,
+        },
+      ],
+    };
+  },
+}).mount("#app");
